3ebf45f9074669ea3e125ee4df4c9106a2f04b1d
albyleo <albertoleo09@outlook.it>
2024-07-02 10:11:30 +0200

    first commit
    Adding files

---
+++ src/main.rs
@@ -0,0 +1,131 @@
+use std::f32::consts::PI;
+use std::time::Duration;
+use camera_controller::{CameraController, CameraControllerPlugin};
+
+use bevy::{
+    animation::{animate_targets, RepeatAnimation},
+    pbr::CascadeShadowConfigBuilder,
+    prelude::*,
+};
+
+fn main() {
+    App::new()
+        .insert_resource(AmbientLight {
+            color: Color::WHITE,
+            brightness: 2000.0,
+        })
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_plugins(CameraControllerPlugin)
+        .add_systems(Update, setup_scene_once_loaded.before(animate_targets))
+        .add_systems(Update, animate_light_direction)
+        .run();
+}
+
+#[derive(Resource)]
+pub struct Animations {
+    animations: Vec<AnimationNodeIndex>,
+    #[allow(dead_code)]
+    graph: Handle<AnimationGraph>,
+}
+mod camera_controller;
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut graphs: ResMut<Assets<AnimationGraph>>,
+) {
+    // Build the animation graph
+    let mut graph = AnimationGraph::new();
+    let animations = graph
+        .add_clips(
+            [GltfAssetLabel::Animation(0).from_asset("ANGEL-FRANK1.glb")]
+                .into_iter()
+                .map(|path| asset_server.load(path)),
+            1.0,
+            graph.root,
+        )
+        .collect();
+
+    // Insert a resource with the current scene information
+    let graph = graphs.add(graph);
+    commands.insert_resource(Animations {
+        animations,
+        graph: graph.clone(),
+    });
+
+    // Camera setup
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 5.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    })
+    .insert(CameraController::default());
+
+    // Light setup
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0, // Adjust the light intensity
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0), // Adjust the light position
+        ..default()
+    });
+
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        cascade_shadow_config: CascadeShadowConfigBuilder {
+            num_cascades: 1,
+            maximum_distance: 1.6,
+            ..default()
+        }
+        .into(),
+        ..default()
+    });
+
+    // Load the glTF scene and start the animation
+    let scene_handle = asset_server.load("ANGEL-FRANK1.glb#Scene0");
+    commands.spawn(SceneBundle {
+        scene: scene_handle.clone(),
+        ..default()
+    });
+}
+
+// Once the scene is loaded, start the animation
+fn setup_scene_once_loaded(
+    mut commands: Commands,
+    animations: Res<Animations>,
+    mut players: Query<(Entity, &mut AnimationPlayer), Added<AnimationPlayer>>,
+) {
+    for (entity, mut player) in &mut players {
+        let mut transitions = AnimationTransitions::new();
+
+        // Start the animation via the `AnimationTransitions` component
+        transitions
+            .play(&mut player, animations.animations[0], Duration::ZERO)
+            .repeat();
+
+        commands
+            .entity(entity)
+            .insert(animations.graph.clone())
+            .insert(transitions);
+    }
+}
+
+fn animate_light_direction(
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<DirectionalLight>>,
+) {
+    for mut transform in &mut query {
+        transform.rotation = Quat::from_euler(
+            EulerRot::ZYX,
+            0.0,
+            time.elapsed_seconds() * std::f32::consts::PI / 50.0,
+            -std::f32::consts::FRAC_PI_4,
+        );
+    }
+}
+
+
